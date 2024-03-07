b30f7313bd42239d85309f5778fe7cc65c3aae42
sheikhabdullahahmed <sheikhabdullahahmed>
2024-03-07 23:06:08 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,566 @@
+
+// let age = 15;
+// if (age >= 18) {
+//     console.log ("Sheikh")
+// }
+
+// if (age <= 18) {
+//     console.log ("Sheikh abdul")
+// }
+
+// let mode ="dark" 
+// let colour ;
+
+// if ( mode === "dark") {
+// colour = "black"
+
+// }
+
+// if (mode === "light") {
+//     colour = "white"
+// } 
+
+// console.log (colour);
+
+
+// let mode = "dark";
+// let colour ;
+
+// if (mode === "dark" ) {
+//     colour = "black"
+// } 
+
+// if (mode ==="light") {
+//     colour= "white"
+// }
+
+// console.log (colour);
+
+
+// let game = "dark";
+// let colour;
+
+// if (game === "dark" ) {
+//     colour = "white"
+// }
+
+// if (game === "blue") {
+//     colour = "red"
+// } 
+// console.log (colour);
+
+// let mode = "black";
+// let colour ;
+
+// if (mode === "white" ) {
+//     colour = "white"
+// }
+
+// else {
+//     colour = "red"
+// }
+// console.log (colour);
+
+
+// let game = "white";
+// let colour ;
+
+// if (game === "white ") {
+//     colour = "white"
+// }
+// else (
+//     colour = "black"
+// )
+
+// console.log (colour);
+
+
+// let age = 25;
+
+// if (age >= 20) {
+//     console.log ("imran")
+// }
+
+// else (
+//     console.log ("vote")
+// )
+
+
+// let mode = "dark";
+// let colour ;
+
+// if (mode === "drak") {
+//     colour = "black"
+// }
+
+// else (
+//     colour = "white"
+// )
+
+// console.log (colour);
+
+// let num = 20;
+
+// if (num  % 2 === 0) {
+//     console.log (num, "is even" );
+// }
+
+// else {
+//     console.log (num, "is odd")
+// }
+
+// let num = 3;
+// if ( num % 2 === 0) {
+//     console.log (num, "is even")
+// } 
+// else {console.log ( num, "is odd")}
+
+
+
+// let num = 8;
+// if (num % 2 === 0 ) {
+//     console.log (num, "is even")
+// }  else (
+//     console.log (num, "is odd")
+// )
+
+
+// let mode = "white";
+// let colour ;
+
+// if (mode === "dark") {
+//   console.log ("black")
+// } 
+// else if (mode === "pink") {
+//     console.log ("pink");
+// }
+
+// else if (mode === "blue") {
+//     console.log ('blue');
+// }
+// else (
+//     colour = "silver"
+// )
+
+// console.log (colour);
+
+
+// let game = "PS5";
+// let pc ;
+
+// if (
+//     game === "RTX"
+// )
+// console.log ("RTX");
+
+// else if (game === "PS5") {
+//     console.log ("PS5");
+// }
+
+// else (
+//     console.log ("laptop")
+// )
+
+// console.log (pc);
+
+// let num =23;
+// let num1;
+
+// if (num === 21) {
+//     console.log (21)
+// }
+
+// else if (num === 22) {
+//     console.log (22);
+// }
+
+// else if( num === 23) {
+//     console.log (23)
+// }
+
+// else (
+//     console.log (40)
+// )
+
+
+// console.log (num1);
+
+
+// let game = "RTX"
+// let pc;
+
+// if (game === "RTX")
+//  {
+//     console.log ("RTX")
+//  }
+
+//  else if  (game === "laptop") {
+//     console.log ("laptop")
+//  }
+
+//  else if (game === "google") {
+//     console.log ("google");
+//  }
+
+//  else {
+//     console.log ("house")
+//  }
+
+// let game = "Gamer"; 
+// let pc ; 
+ 
+// if (game === "kkr") {
+//     console.log ("Gamer")
+// }
+
+// else if (
+//     game === 'house'
+// ){
+//     console.log ("house")
+// }
+
+// else if (game === "hoor") {
+//     console.log ("hHoor")
+// }
+
+// else {
+//     console.log ("Poka")
+// }
+
+// let fullName = "lll";
+// let AnotherName = "lll";
+
+// if (fullName === "Sheikh Abdullah") {
+//     console.log ("Sheikh Abdullah")
+// }
+
+// else if (AnotherName === "Abdullah") {
+//     console.log ("Abdullah")
+// }
+
+// else if (AnotherName === "Sheikh") {
+//     console.log ("Sheikh")
+// }
+
+// else {
+//     console.log ("Shapatar")
+// }
+// let vote = 20;
+// if (vote < 20) {
+//     alert("You are Voter")
+// } 
+// else{
+//     alert ("you are Not Voter")
+// // }
+// const fooditems = {
+//     Burger : 149,
+//     Pizza  : 160,
+//     Taco : 140,
+// }
+
+// function foodPrice(items) {
+//     return foodPrice[items]
+// }
+ 
+
+// let fullName = "kkk";
+// let UserName = "A";
+// let AnotherName = "S";
+
+
+// if (fullName === "S") {
+//     console.log ("Sheikh Abdullah")
+// }
+
+// else if (UserName === "Abdullah") {
+//     console.log ("b")
+// }
+
+// else {
+//     console.log ("Shapatar")
+// }
+
+// let fullName = "Sheikh";
+// let UserName = "Abdullah";
+// let AnotherName = "Sheikh Abdullah";
+
+// if (fullName == "Sheikh ") {
+//     console.log ("Sheikh Abdullah")
+// }
+// else if (UserName == "Abdullah ") {
+//     console.log ("Gamer")
+// }
+// else {
+//     alert("poka pur")
+// }
+
+// let fullName = "Sheikh Abdullah";
+// let UserName = "Sheikh Abdullah Ahmed";
+// let AnotherName = "Abdullah";
+
+
+// if (fullName == " Abdullah") {
+//     alert("Despacito")
+// }
+// else if (UserName == " Ahmed") {
+//     alert("Gamer")
+// }
+// else {
+//     alert("Poka Pur")
+// }
+
+// let fullName = "Sheikh Abdullah"; 
+// let lastName = "Ahmed";
+// let AnotherName = "Ustad";
+
+// if (fullName == "Sheikh ") {
+//     alert ("Sheikh Abdullah Abdullah")
+// }else if (lastName == "Ad") {
+//     alert("Ahemd Ahmed")
+// }
+// else {
+//     alert("Laptop")
+// }
+
+// let vote = 20;
+// let vote2 = 18;
+// if (vote > 200) {
+//     alert("Abdullah")
+// }
+// else if (vote < )
+// let vote =  prompt ("Enter Your Age 1");
+// let vote2 = prompt ("Enter Your Age 2")
+// if (vote < vote2) {
+//     alert("Mubarak ho")
+// } else {
+//     alert("Gando")
+// }
+
+
+// let age = prompt ('Enter Your Age');
+// if (age <= 20) {
+//     alert ("True")
+// }else {
+//     alert ("false")
+// }
+
+
+// let age = prompt("What is Your Age");
+// let gift = prompt ("Gift liya ho? type yse agar li A ho");
+// let cake = prompt ("Cake Kaeo gey? type yes  agar laye ho");
+
+// if (age <= 10 && gift === "Yes" || cake == "no" ){
+//     console.log ('yes')
+// }
+
+// let age = prompt ("What is Your age");
+// let gift = prompt ("gift liya ho");
+// let cake = prompt ("Cake lay kar ay ho");
+
+// if (age <= 20 && gift === "yes" || cake === "No" ) {
+//     console.log ("YEyyy budddy")
+// } else{
+//     alert("kkkkkkk")
+// }
+
+
+
+// let x = 10; 
+// let y = 100; 
+// if (x == y) {
+//     alert("You are  begineer")
+// }
+// else {
+//     alert("You are wrong")
+// }
+
+// let age = 20; 
+// if (age >=20) {
+//     return true;
+
+// }
+
+
+// console.log (age)
+
+
+
+// function age (max) {
+//     let age = 2 
+//     if (age >=20 ) {
+//         return true;
+//     }else {
+//         return false;
+//     }
+
+// }
+// console.log (age("max"))
+// let num = 20; 
+// if (num >= 100) {
+//     alert("Rigth")
+// }else {
+//     alert("wrong")
+// }
+
+// let num = 20; 
+// if (num >= 2) {
+//     alert("GAMER")
+// } 
+// else {
+//     alert("Thanks")
+// }
+
+
+//  opperattor 
+
+
+
+
+
+
+
+// let age  =25;
+
+
+// let result = age >= 18 ? "adult"  : "not adult" ;
+
+// console.log (result);
+
+
+// let num = 30;
+
+
+// let result = num >= 40 ? "game"  : "not game" ;
+// console.log (result);
+
+// let num = prompt('Enter Your Number');
+
+
+// if (num % 5===0 ) {
+//     console.log (num, "is Multiple Of 5")
+// }
+
+// else (
+//     console.log (num, "is not Multiple Of 5")
+// )
+
+
+
+// let game = prompt("vice city");
+
+// if (game % 7 === 0) {
+//     console.log ("this is good")
+//
+// else (
+//     console.log ("this is not good")
+// )
+
+// let scrore = 50;
+// let grage;
+
+// if (scrore >= 90 && scrore <= 100) {
+//     console.log ("grade A")
+// }
+// else if (scrore >= 70 && scrore<= 80) {
+//     console.log ("grade B")
+// }
+
+// else if (scrore >= 50 && scrore <= 60) {
+//     console.log ("grade C")
+// }
+
+// else (
+//     console.log ("grade F")
+// )
+
+
+// console.log (grage);
+
+
+// let game = '40';
+// let result ;
+
+// if (game >= 10 && game <=20 ) {
+//     console.log ("grade A")
+// }
+// else if (game >=21  && game <= 39) {
+//     console.log ("grade B")
+// }
+// else  if(game >= 40 && game <= 50) {
+//     console.log ("grade C")
+// }
+
+// console.log (result);
+
+// let hum = "45";
+// let result;
+
+// if (hum >= 30 && hum <= 40) {
+//     console.log ("grade A")
+// }
+
+// if (hum >= 41 && hum <= 50) {
+//     console.log ("grade ")
+// }
+
+// console.log (result);
+
+
+// let score = +prompt ("Enter Your Score");
+// let grade;
+
+// if (
+//     score >= 90 && score <=100
+// ) {
+//     console.log ("grade A")
+// }
+//  else if (score >= 70 && score <= 89) {
+//     console.log ("grade B")
+// }
+//  else if (score >= 60 && score<= 69) {
+//     console.log ("grade C")
+// }
+//  else if (score>= 50 && score<= 59) {
+//     console.log ("grade F")
+// }
+
+// console.log (grade)
+
+
+// let score = prompt ("Enter Your Scrore"); 
+// let grade ; 
+
+// if (score >= 90 && score <=100) {
+//     console.log ("grade A")
+// }
+
+// else if (score >= 70 && score <= 89 ) {
+//     console.log ("grade B")
+// }
+
+// else if (score >= 50 && score<= 59) {
+//     console.log ("grade C")
+// }
+
+// else {
+//     console.log ("grade D")
+// }
+
+
+// let score = prompt ("Enter Your Number"); 
+// let grade ; 
+// if (score >= 90 && score <= 100) {
+//     console.log ('grade A')
+// }
+
+// else if (score <=80 && score >=70 ) {
+//     console.log ('grade B')
+// }
+
+// else if (score <= 60 && score >= 50) {
+//     console.log ("grade C")
+// }
+
+// else {
+//     console.log ("Failuier")
+// }
